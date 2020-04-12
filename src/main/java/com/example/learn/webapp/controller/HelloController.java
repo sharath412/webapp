77405be51afecf1e63a3172f35c607806495ea90
Sharath Mekkottil <sharath.mekkottil@cleartrip.com>
2020-04-12 14:04:08 +0530

    new controller for test

--- src/main/java/com/example/learn/webapp/controller/HelloController.java
+++ src/main/java/com/example/learn/webapp/controller/HelloController.java
@@ -1,14 +1,20 @@
 package com.example.learn.webapp.controller;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class HelloController {
 
 	
-	@GetMapping
+	@GetMapping("/hello")
 	public String hello() {
 		return "Hello World";
+	}
+	
+	@GetMapping("/hello/{name}")
+	public String helloFriend(@PathVariable("name")String name) {
+		return "Hello "+name;
 	}
 }
